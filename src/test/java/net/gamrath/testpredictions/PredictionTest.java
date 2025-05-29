d1cbefee235bec443011f123cbcbaf41c6eaf284
Christoffer Gamrath <chris@gamrath.net>
2025-05-29 13:57:18 +0200

    Add tests for checking prediction

---
+++ src/test/java/net/gamrath/testpredictions/PredictionTest.java
@@ -0,0 +1,32 @@
+package net.gamrath.testpredictions;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static net.gamrath.testpredictions.Prediction.ALL_PASS;
+import static net.gamrath.testpredictions.Prediction.ANY_FAIL;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class PredictionTest {
+    @Test
+    void allPassHit() {
+        assertTrue(ALL_PASS.test(List.of(TestResult.PASS, TestResult.PASS)));
+    }
+
+    @Test
+    void allPassMiss() {
+        assertFalse(ALL_PASS.test(List.of(TestResult.PASS, TestResult.FAIL)));
+    }
+
+    @Test
+    void anyFailHit() {
+        assertTrue(ANY_FAIL.test(List.of(TestResult.PASS, TestResult.FAIL)));
+    }
+
+    @Test
+    void anyFailMiss() {
+        assertFalse(ANY_FAIL.test(List.of(TestResult.PASS, TestResult.PASS)));
+    }
+}
