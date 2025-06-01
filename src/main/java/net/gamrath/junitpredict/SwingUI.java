a84893c6a5fa8672d45edde6124471964c5c0d6d
Christoffer Gamrath <chris@gamrath.net>
2025-06-01 11:26:27 +0200

    Remove duplication

--- src/main/java/net/gamrath/junitpredict/SwingUI.java
+++ src/main/java/net/gamrath/junitpredict/SwingUI.java
@@ -8,33 +8,20 @@ class SwingUI implements UI {
     @Override
     public Prediction promptForPrediction() {
         final var prediction = new AtomicReference<Prediction>();
-        try {
-            SwingUtilities.invokeAndWait(() -> {
-                final var choice = JOptionPane.showOptionDialog(
-                        null,
-                        "Do you predict that ALL tests will PASS or that ANY will FAIL?",
-                        "Call your shot!",
-                        JOptionPane.DEFAULT_OPTION,
-                        JOptionPane.QUESTION_MESSAGE,
-                        null,
-                        new Object[]{"FAIL", "PASS"},
-                        null
-                );
-                Prediction result;
-                if (choice == 0) {
-                    result = Prediction.ANY_FAIL;
-                } else if (choice == 1) {
-                    result = Prediction.ALL_PASS;
-                } else if (choice == JOptionPane.CLOSED_OPTION) {
-                    result = Prediction.SKIP;
-                } else {
-                    throw new IllegalStateException("No prediction made. result=%d".formatted(choice));
-                }
-                prediction.set(result);
-            });
-        } catch (InterruptedException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
+        invokeUnchecked(() -> {
+            final var choice = JOptionPane.showOptionDialog(null, "Do you predict that ALL tests will PASS or that ANY will FAIL?", "Call your shot!", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, new Object[]{"FAIL", "PASS"}, null);
+            Prediction result;
+            if (choice == 0) {
+                result = Prediction.ANY_FAIL;
+            } else if (choice == 1) {
+                result = Prediction.ALL_PASS;
+            } else if (choice == JOptionPane.CLOSED_OPTION) {
+                result = Prediction.SKIP;
+            } else {
+                throw new IllegalStateException("No prediction made. result=%d".formatted(choice));
+            }
+            prediction.set(result);
+        });
         return prediction.get();
     }
 
