9b5274e64df8c7037fad27091da2ba0673e6607e
Christoffer Gamrath <chris@gamrath.net>
2025-06-01 12:59:59 +0200

    Extract

---
+++ src/main/java/net/gamrath/junitpredict/Logger.java
@@ -0,0 +1,34 @@
+package net.gamrath.junitpredict;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Logger {
+    static ArrayList<String> createLog(List<String> lines, boolean hit, final Prediction prediction) {
+        var hits = 0;
+        var misses = 0;
+        for (final var line : lines) {
+            final var parts = line.split(",");
+            final var isHit = Boolean.parseBoolean(parts[1]);
+            if (isHit) {
+                hits++;
+            } else {
+                misses++;
+            }
+        }
+
+        final var log = formatLogLine(prediction, hit);
+        final var newLines = new ArrayList<>(lines);
+        newLines.add(log);
+        newLines.add(formatStatsLine(hits + (hit ? 1 : 0), misses + (hit ? 0 : 1)));
+        return newLines;
+    }
+
+    private static String formatLogLine(final Prediction prediction, boolean hit) {
+        return "%s,%s".formatted(prediction, hit);
+    }
+
+    private static String formatStatsLine(final int hits, final int misses) {
+        return "STATS: hits=%d, misses=%d".formatted(hits, misses);
+    }
+}
